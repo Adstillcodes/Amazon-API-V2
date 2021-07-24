c16497a3b2d43c028efc056496f8f2ea57dbfe97
Anvit3 <anvit5.deshpande@gmail.com>
2021-07-24 19:06:52 +0530

    make it better

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
 const express = require('express');
 const request = require('request-promise');
 
-const app = express();
 const PORT = process.env.PORT || 5000;
-
-
-const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Amazon Scraper API');
+const returnScraperApiUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+// Welcome route
+app.get('/', async (req, res) => {
+    res.send('Welcome to Amazon Scraper API!');
 });
 
-// Endpoint 1 GET Product Details
+// Get product details
 app.get('/products/:productId', async (req, res) => {
     const { productId } = req.params;
     const { api_key } = req.query;
 
-    try{
-        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.in/dp/${productId}`)
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
+        
         res.json(JSON.parse(response));
-    }   catch(error) {
+    } catch (error) {
         res.json(error);
     }
-})
-//End Endpoint 1
+});
 
-// Endpoint 2  GET Product Reviews
+// Get product reviews
 app.get('/products/:productId/reviews', async (req, res) => {
     const { productId } = req.params;
     const { api_key } = req.query;
-
-    try{
-        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.in/product-reviews/${productId}`)
+    
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`);
+        
         res.json(JSON.parse(response));
-    }   catch(error) {
+    } catch (error) {
         res.json(error);
     }
-})
-//End Endpoint 2
+});
 
-// Endpoint 3  GET Product Offers
+// Get product offers
 app.get('/products/:productId/offers', async (req, res) => {
     const { productId } = req.params;
     const { api_key } = req.query;
-
-    try{
-        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.in/gp/offer-listing/${productId}`)
+    
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+        
         res.json(JSON.parse(response));
-    }   catch(error) {
+    } catch (error) {
         res.json(error);
     }
-})
-//End Endpoint 3
+});
 
-// Endpoint 4  GET Search Reasults
+// Get search results
 app.get('/search/:searchQuery', async (req, res) => {
     const { searchQuery } = req.params;
     const { api_key } = req.query;
-
-    try{
-        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.in/s?k=${searchQuery}`)
+    
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+        
         res.json(JSON.parse(response));
-    }   catch(error) {
+    } catch (error) {
         res.json(error);
     }
-})
-//End Endpoint 4
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
